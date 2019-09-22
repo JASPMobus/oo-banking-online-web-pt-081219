dae207df9cd730550577298c02ebc368903ef438
James Mobus <mobusj@live.com>
2019-09-22 17:46:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,5 @@
 class Transfer
-  attr_accessor :sender, :receiever, :status, :amount
+  attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, receiever, amount)
     @sender = sender
